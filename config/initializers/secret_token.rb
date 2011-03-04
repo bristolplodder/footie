b8c1ef919826729359c062cb67acdf9320b12adc
Matthew Cockburn <matthew.cockburn@blueyonder.co.uk>
2011-03-04 20:36:58 +0000

    Input table

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Footie::Application.config.secret_token = 'cdfabd840c20b90a7080c329f64a2a80d7c6026d3a51712a7e549163ba727ed85802e4b57252a52110850265703973c44d4ee4186ba17ccfc9a612004e7e1c11'
